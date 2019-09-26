373235543f5a892d56b9be4019c63a6def18ae59
Alin Popa <alin.popa@fxdata.ro>
2019-09-26 21:19:02 +0200

    Update email address to personal email

--- crashmanager/cdm-janitor.c
+++ crashmanager/cdm-janitor.c
@@ -17,7 +17,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * \author Alin Popa <alin.popa@bmw.de>
+ * \author Alin Popa <alin.popa@fxdata.ro>
  * \file cdm-janitor.c
  */
 
